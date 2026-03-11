66148914df17d423a20dfdff4ebfd40095a335af
axelbrons <axel.brons@edu.ece.fr>
2026-03-11 11:57:01 +0000

    feat: add roc curve in test.py

--- src/test.py
+++ src/test.py
@@ -10,6 +10,8 @@ from transformers import AutoTokenizer
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, confusion_matrix
 from tqdm import tqdm
+from sklearn.metrics import roc_curve, auc
+import torch.nn.functional as F
 
 from src import config
 from src.dataset import SolidityDataset
@@ -50,6 +52,7 @@ def test():
     # 3. Evaluation
     all_preds = []
     all_labels = []
+    all_probs = []
     test_bar = tqdm(test_loader, desc="Testing")
 
     with torch.no_grad():
@@ -63,6 +66,9 @@ def test():
                 logits = model(input_ids, attention_mask)
                 
             preds = torch.argmax(logits, dim=1)
+
+            probs = F.softmax(logits.float(), dim=1)[:, 1] # Probabilité de la classe 1 (Vulnerable)
+            all_probs.extend(probs.cpu().numpy())
             
             all_preds.extend(preds.cpu().numpy())
             all_labels.extend(labels.cpu().numpy())
@@ -74,6 +80,9 @@ def test():
 
     print("\nClassification Report:")
     print(classification_report(all_labels, all_preds, target_names=target_names))
+
+    fpr, tpr, _ = roc_curve(all_labels, all_probs)
+    roc_auc = auc(fpr, tpr)
 
     # 5. Confusion Matrix Visualization
     cm = confusion_matrix(all_labels, all_preds)
@@ -87,6 +96,15 @@ def test():
     plt.tight_layout()
     plt.savefig('confusion_matrix.png')
     print("Confusion matrix saved as confusion_matrix.png")
+
+    plt.figure()
+    plt.plot(fpr, tpr, color='darkorange', lw=2, label=f'ROC curve (AUC = {roc_auc:.2f})')
+    plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver Operating Characteristic (ROC)')
+    plt.legend(loc="lower right")
+    plt.savefig('roc_curve.png')
     # plt.show() # Sur un serveur distant comme Onyxia, plt.show() peut parfois bloquer, on commente par précaution.
 
 if __name__ == "__main__":
